f6113b415318316862e5c086e0854f678452c673
pseudosurge <167565213+pseudosurge@users.noreply.github.com>
2024-04-23 11:37:09 -0500

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,94 @@
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
-	
+    if (controller.right.isPressed()) {
+        hamshot = sprites.createProjectileFromSprite(img`
+            . . . . . . 2 2 2 2 . . . . . . 
+            . . . . 2 2 3 3 3 3 2 e . . . . 
+            . . . 2 3 d 1 1 d d 3 2 e . . . 
+            . . 2 3 1 d 3 3 3 d d 3 e . . . 
+            . 2 3 1 3 3 3 3 3 d 1 3 b e . . 
+            . 2 1 d 3 3 3 3 d 3 3 1 3 b b . 
+            2 3 1 d 3 3 1 1 3 3 3 1 3 4 b b 
+            2 d 3 3 d 1 3 1 3 3 3 1 3 4 4 b 
+            2 d 3 3 3 1 3 1 3 3 3 1 b 4 4 e 
+            2 d 3 3 3 1 1 3 3 3 3 1 b 4 4 e 
+            e d 3 3 3 3 d 3 3 3 d d b 4 4 e 
+            e d d 3 3 3 d 3 3 3 1 3 b 4 b e 
+            e 3 d 3 3 1 d d 3 d 1 b b e e . 
+            . e 3 1 1 d d 1 1 1 b b e e e . 
+            . . e 3 3 3 3 3 3 b e e e e . . 
+            . . . e e e e e e e e e e . . . 
+            `, mySprite, 200, 0)
+    } else {
+        if (controller.left.isPressed()) {
+            hamshot = sprites.createProjectileFromSprite(img`
+                . . . . . . 2 2 2 2 . . . . . . 
+                . . . . 2 2 3 3 3 3 2 e . . . . 
+                . . . 2 3 d 1 1 d d 3 2 e . . . 
+                . . 2 3 1 d 3 3 3 d d 3 e . . . 
+                . 2 3 1 3 3 3 3 3 d 1 3 b e . . 
+                . 2 1 d 3 3 3 3 d 3 3 1 3 b b . 
+                2 3 1 d 3 3 1 1 3 3 3 1 3 4 b b 
+                2 d 3 3 d 1 3 1 3 3 3 1 3 4 4 b 
+                2 d 3 3 3 1 3 1 3 3 3 1 b 4 4 e 
+                2 d 3 3 3 1 1 3 3 3 3 1 b 4 4 e 
+                e d 3 3 3 3 d 3 3 3 d d b 4 4 e 
+                e d d 3 3 3 d 3 3 3 1 3 b 4 b e 
+                e 3 d 3 3 1 d d 3 d 1 b b e e . 
+                . e 3 1 1 d d 1 1 1 b b e e e . 
+                . . e 3 3 3 3 3 3 b e e e e . . 
+                . . . e e e e e e e e e e . . . 
+                `, mySprite, -200, 0)
+        } else {
+            if (controller.down.isPressed()) {
+                hamshot = sprites.createProjectileFromSprite(img`
+                    . . . . . . 2 2 2 2 . . . . . . 
+                    . . . . 2 2 3 3 3 3 2 e . . . . 
+                    . . . 2 3 d 1 1 d d 3 2 e . . . 
+                    . . 2 3 1 d 3 3 3 d d 3 e . . . 
+                    . 2 3 1 3 3 3 3 3 d 1 3 b e . . 
+                    . 2 1 d 3 3 3 3 d 3 3 1 3 b b . 
+                    2 3 1 d 3 3 1 1 3 3 3 1 3 4 b b 
+                    2 d 3 3 d 1 3 1 3 3 3 1 3 4 4 b 
+                    2 d 3 3 3 1 3 1 3 3 3 1 b 4 4 e 
+                    2 d 3 3 3 1 1 3 3 3 3 1 b 4 4 e 
+                    e d 3 3 3 3 d 3 3 3 d d b 4 4 e 
+                    e d d 3 3 3 d 3 3 3 1 3 b 4 b e 
+                    e 3 d 3 3 1 d d 3 d 1 b b e e . 
+                    . e 3 1 1 d d 1 1 1 b b e e e . 
+                    . . e 3 3 3 3 3 3 b e e e e . . 
+                    . . . e e e e e e e e e e . . . 
+                    `, mySprite, 0, 200)
+            } else {
+                if (controller.up.isPressed()) {
+                    hamshot = sprites.createProjectileFromSprite(img`
+                        . . . . . . 2 2 2 2 . . . . . . 
+                        . . . . 2 2 3 3 3 3 2 e . . . . 
+                        . . . 2 3 d 1 1 d d 3 2 e . . . 
+                        . . 2 3 1 d 3 3 3 d d 3 e . . . 
+                        . 2 3 1 3 3 3 3 3 d 1 3 b e . . 
+                        . 2 1 d 3 3 3 3 d 3 3 1 3 b b . 
+                        2 3 1 d 3 3 1 1 3 3 3 1 3 4 b b 
+                        2 d 3 3 d 1 3 1 3 3 3 1 3 4 4 b 
+                        2 d 3 3 3 1 3 1 3 3 3 1 b 4 4 e 
+                        2 d 3 3 3 1 1 3 3 3 3 1 b 4 4 e 
+                        e d 3 3 3 3 d 3 3 3 d d b 4 4 e 
+                        e d d 3 3 3 d 3 3 3 1 3 b 4 b e 
+                        e 3 d 3 3 1 d d 3 d 1 b b e e . 
+                        . e 3 1 1 d d 1 1 1 b b e e e . 
+                        . . e 3 3 3 3 3 3 b e e e e . . 
+                        . . . e e e e e e e e e e . . . 
+                        `, mySprite, 0, -200)
+                } else {
+                    mySprite.startEffect(effects.ashes)
+                }
+            }
+        }
+    }
 })
+let hamshot: Sprite = null
+let mySprite: Sprite = null
 tiles.setCurrentTilemap(tilemap`level2`)
-let mySprite = sprites.create(img`
+mySprite = sprites.create(img`
     .......fffff..........
     .....ffff11fff........
     .fffffff11111fff......
@@ -53,21 +139,3 @@ statusbar.setLabel("HP")
 statusbar2.attachToSprite(mySprite2)
 tiles.placeOnRandomTile(mySprite2, sprites.dungeon.floorDarkDiamond)
 scene.cameraFollowSprite(mySprite)
-let projectile = sprites.create(img`
-    . . . . . . 2 2 2 2 . . . . . . 
-    . . . . 2 2 3 3 3 3 2 e . . . . 
-    . . . 2 3 d 1 1 d d 3 2 e . . . 
-    . . 2 3 1 d 3 3 3 d d 3 e . . . 
-    . 2 3 1 3 3 3 3 3 d 1 3 b e . . 
-    . 2 1 d 3 3 3 3 d 3 3 1 3 b b . 
-    2 3 1 d 3 3 1 1 3 3 3 1 3 4 b b 
-    2 d 3 3 d 1 3 1 3 3 3 1 3 4 4 b 
-    2 d 3 3 3 1 3 1 3 3 3 1 b 4 4 e 
-    2 d 3 3 3 1 1 3 3 3 3 1 b 4 4 e 
-    e d 3 3 3 3 d 3 3 3 d d b 4 4 e 
-    e d d 3 3 3 d 3 3 3 1 3 b 4 b e 
-    e 3 d 3 3 1 d d 3 d 1 b b e e . 
-    . e 3 1 1 d d 1 1 1 b b e e e . 
-    . . e 3 3 3 3 3 3 b e e e e . . 
-    . . . e e e e e e e e e e . . . 
-    `, SpriteKind.Projectile)
